83b3b85bcf84bf126037d1583c6327785d107ec3
Kiattisak Chaisomboon <bass.kiattisak@gmail.com>
2020-05-19 15:12:09 +0700

    update

--- plot2.R
+++ plot2.R
@@ -18,7 +18,7 @@ dataset <- subset(dataset, Date=='1/2/2007' | Date=='2/2/2007')
 dataset <- mutate(dataset, DateTime = paste(Date, Time))
 dataset <- mutate(dataset, DateTime = as.POSIXct(strptime(DateTime, '%d/%m/%Y %H:%M:%S'), tz=''))
 
-## plot1
-png("figure/plot1.png", width = 480, height = 480)
-hist(dataset$Global_active_power, main='Global Active Power', xlab='Global Active Power (kilowatts)', ylim=c(1,1200), col='red')
+## plot2
+png("figure/plot2.png", width = 480, height = 480)
+plot(dataset$DateTime, dataset$Global_active_power, type = "S", xlab = '', ylab = 'Global Active Power (kilowatts)')
 dev.off()
