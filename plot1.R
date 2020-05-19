c9a5aba89a5ef3f12c13035004da1afc25548441
Kiattisak Chaisomboon <bass.kiattisak@gmail.com>
2020-05-19 10:01:04 +0700

    Update R Script

--- plot1.R
+++ plot1.R
@@ -1,14 +1,29 @@
 
-## Download and Unzip file
-download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', destfile = 'household_power_consumption.zip')
-unzip('household_power_consumption.zip')
+## Check text file
+files <- list.files(pattern = "household_power_consumption.txt")
+if(length(files) == 0){
+  ## Download and Unzip file
+  download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', destfile = 'household_power_consumption.zip')
+  unzip('household_power_consumption.zip')
+}
+rm(files)
 
 ## Read Text File to Dataset
-dataset <- read.table('household_power_consumption.txt', sep=';', na.strings = '?', stringsAsFactors = FALSE)
-names(dataset) <- dataset[1, ]
-dataset <- dataset[-1,]
+dataset <- read.table('household_power_consumption.txt', header=TRUE, sep=';', 
+                      na.strings = '?', stringsAsFactors = FALSE)
 
 ## Create New Column 'DateTime' which combine of 'Date' column and 'Time' Column
 library(dplyr)
 dataset <- subset(dataset, Date=='1/2/2007' | Date=='2/2/2007')
-#dataset <- mutate(dataset, DateTime = paste(Date, Time))
+dataset <- mutate(dataset, DateTime = paste(Date, Time))
+dataset <- mutate(dataset, DateTime = as.POSIXct(strptime(DateTime, '%d/%m/%Y %H:%M:%S'), tz=''))
+
+## plot1
+png("figure/plot1.png", width = 480, height = 480)
+hist(dataset$Global_active_power, main='Global Active Power', xlab='Global Active Power (kilowatts)', ylim=c(1,1200))
+dev.off()
+
+## plot2
+png("figure/plot2.png", width = 480, height = 480)
+plot(dataset$DateTime, dataset$Global_active_power, type = "S", xlab = '', ylab = 'Global Active Power (kilowatts)')
+dev.off()
