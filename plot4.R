04d415fb898b0fd8f181f18a0f905f72fc879a16
Kiattisak Chaisomboon <bass.kiattisak@gmail.com>
2020-05-19 17:02:45 +0700

    update

--- plot4.R
+++ plot4.R
@@ -18,11 +18,12 @@ dataset <- subset(dataset, Date=='1/2/2007' | Date=='2/2/2007')
 dataset <- mutate(dataset, DateTime = paste(Date, Time))
 dataset <- mutate(dataset, DateTime = as.POSIXct(strptime(DateTime, '%d/%m/%Y %H:%M:%S'), tz=''))
 
-## Create plot4.png
+## Create plot4.png and set subplot
 png("figure/plot4.png", width = 480, height = 480)
+par(mfrow=c(2,2))
 
 ## Subplot (1, 1)
-with(dataset, plot(DateTime, Global_active_power, type = "S", xlab = '', ylab = 'Global Active Power (kilowatts)'))
+with(dataset, plot(DateTime, Global_active_power, type = "S", xlab = '', ylab = 'Global Active Power'))
 
 ## Subplot (1, 2)
 with(dataset, plot(DateTime, Voltage, type = "S", xlab = 'datetime', ylab = 'Voltage'))
@@ -31,7 +32,7 @@ with(dataset, plot(DateTime, Voltage, type = "S", xlab = 'datetime', ylab = 'Vol
 with(dataset, plot(DateTime, Sub_metering_1, type = "S", xlab = '', ylab = 'Energy sub metering'))
 with(dataset, lines(DateTime, Sub_metering_2, col='red'))
 with(dataset, lines(DateTime, Sub_metering_3, col='blue'))
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, cex=0.8)
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, bty = "n", cex=0.8)
 
 ## Subplot (2, 2)
 with(dataset, plot(DateTime, Global_reactive_power, type = "S", xlab = 'datetime', ylab = 'Global_reactive_power'))
